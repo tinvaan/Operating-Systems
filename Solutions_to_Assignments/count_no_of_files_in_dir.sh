5753a6c1f09b2608ef72c32b30c508ed39b4ae4a
R.Harish Navnit <harishnavnit@gmail.com>
2014-07-26 22:37:23 +0530

    Deleting the right files

--- Solutions_to_Assignments/count_no_of_files_in_dir.sh
+++
@@ -1,9 +0,0 @@
-#
-# A simple shell script to count the no. of files in a given directory
-#
-
-read -p "Enter the directory you want to look into(Make sure you enter the complete path) : " path_to_dir
-cd $path_to_dir
-count=$(ls -l | wc -l)
-echo "The no. of files in ${path_to_dir} is : $count"
-echo
--- Solutions_to_Assignments/greet.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash 
-# Shell script that reads input from the user.
-#
-
-read -p "Enter your name : " name     # Read the input and store in $name
-echo "Hi, $name. Good to see you !"
-echo
-
